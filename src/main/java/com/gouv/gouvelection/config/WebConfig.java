3b0e391d1b5994dd346c177280eec05d6bb6676e
VascoII <fabriceds@yahoo.fr>
2023-11-17 12:49:38 +0100

    env file added

--- src/main/java/com/gouv/gouvelection/config/WebConfig.java
+++ src/main/java/com/gouv/gouvelection/config/WebConfig.java
@@ -8,13 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    private String appOrigin = System.getenv("ALLOWED_ORIGINS_URL");
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:1234")
+                        .allowedOrigins(appOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
